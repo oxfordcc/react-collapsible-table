8dc62f7362dc87de0f453c663903f1127b313b87
vanilinka33@gmail.com <alexbokii@phaedra.occ.lan>
2016-08-04 16:03:32 +0100

    initial commit

---
+++ src/CollapsibleRow.jsx
@@ -0,0 +1,9 @@
+var CollapsibleRow = React.createClass({
+    render: function () {
+        return (
+            <div>
+                Some expandable data
+            </div>
+        );
+    }
+});
